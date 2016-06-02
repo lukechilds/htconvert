9a7b36564179677f670859729d148f2d8570008a
Luke Childs <lukechilds123@gmail.com>
2016-06-02 21:23:11 +0100

    Add input/output fixtures with test

--- test/unit.js
+++ test/unit.js
@@ -1,7 +1,15 @@
 import test from 'ava';
 
+import { readFileSync } from 'fs';
 import htconvert from '../dist/htconvert';
+
+const input = readFileSync('./fixtures/input', 'utf-8');
+const output = readFileSync('./fixtures/output', 'utf-8');
 
 test('htconvert should export a function', t => {
   t.is(typeof htconvert, 'function');
+});
+
+test('htconvert should match input with output', t => {
+  t.is(htconvert(input), output);
 });
